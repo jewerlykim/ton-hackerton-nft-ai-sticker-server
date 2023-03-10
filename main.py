a859be104c1839cb17cba4dc41d468cf703491b6
JewelryKim <jsjs21good@gmail.com>
2023-03-11 01:27:57 +0900

    mask 바꾸고, jpeg 로 변경

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from pydantic import BaseModel
 import io
 import os
 import requests
+from PIL import Image
+from fastapi.responses import StreamingResponse
+import requests
 
 class PredictBody(BaseModel):
     prompt: str
@@ -208,10 +211,14 @@ async def batch_predict(
         #     f.write(response.content)
 
         # Return the output image as a response
-        results.extend(outputs)
+        # results.extend(outputs)
         # results.append(outputs)
+        url = outputs[0]
+        print("url: ", url)
+        return await compress_image(url)
         
-    return results
+    # 실패
+    return {"message": "failed"} 
 
 
 @app.post("/batch_predict_test")
@@ -236,7 +243,7 @@ async def batch_predict_test(
     # 사랑: 분홍색 배경과 큰 눈, 미소짓는 입으로 캐릭터를 표현할 수 있습니다.
     # 축하: 파란색 배경과 큰 눈, 손에 선물 상자를 들고 있는 표정으로 캐릭터를 표현할 수 있습니다.
     # 신나는: 오렌지색 배경과 광대한 눈, 미소 짓고 손을 흔드는 표정으로 캐릭터를 표현할 수 있습니다.
-    base_positive_prompts = "(body:2.0), (pixar style:1.0), 8k, High Detail, 3D, (1girl:2.0),(1person:2.0), simple hair, no hair band, (girl:1.0)"
+    base_positive_prompts = "(body:2.0), (pixar style:1.0), 8k, High Detail, 3D, (1girl:2.0),(1person:2.0), simple hair, no hair band, (girl:1.0), arm"
     base_negative_prompts = "disfigured, bad art, extra fingers, mutated hands, blurry, no arms, bad anatomy, bad hair, arms, Accessories, (hair band:1.0), hat, hoodie, cap, glowing hair, people"
 
     results = ["https://replicate.delivery/pbxt/fa1tlGTIL5xZUaRPXLDfEeHWC1saiSF8wtHlhLFXywtaCaLhA/out-0.png"]
@@ -301,3 +308,26 @@ async def timelock(x: int):
     print("timelock called with x: ", x)
     time.sleep(x)
     return {"result": "success"}
+
+
+
+# image url 을 받으면 용량 압축해주는 함수
+async def compress_image(image_url: str, quality: int = 100):
+    image_data = None
+    # image 다운로드
+    response = requests.get(image_url)
+
+    # image open
+    image = Image.open(io.BytesIO(response.content))
+
+    # compress
+    compressed_image_bytes = io.BytesIO()
+    image.save(compressed_image_bytes, format='jpeg', optimize=True, quality=quality)
+    compressed_image_bytes = compressed_image_bytes.getvalue()
+
+    # 용량 출력
+    print("original image size: ", len(response.content))
+    print("compressed image size: ", len(compressed_image_bytes))
+
+    #send the compressed image back to the user
+    return StreamingResponse(io.BytesIO(compressed_image_bytes), media_type="image/jpeg")
